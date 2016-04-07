50ec2580b026a3eeb694550538f3153db3099622
fej <ff.jj@gmx.at>
2016-04-07 22:46:41 +0200

    Added Track management functions
    Tracks now get inserted at the correct position. Also added a function
    to move positions of tracks.

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,11 +1,26 @@
 $('html').removeClass('no-js');
 $('html').addClass('js');
 
+// contains score values of all tracks in the order they appear in #tracklist
+var scores = [];
+
 var tNowPl;
 var tTrackL;
 
+// adds track (object) to the tracklist at the correct position
 function addTrack (track) {
-	// create html
+	// find position to insert
+	var i = 0;
+	while (scores[i] != undefined && // stop at end of list
+		track.score > scores[i]) // stop when track score is better than current score
+		i++;
+	
+	insertTrackAt(createTrackHtml(track), track.score, i);
+}
+
+// returns jQuery element containing li element to insert into tracklist
+function createTrackHtml (track) {
+	// create html element
 	var artistString = '';
 	track.artists.forEach(function (artist) {
 		if (artistString)
@@ -14,17 +29,63 @@ function addTrack (track) {
 			artistString = artist.name;
 	});
 	
-	var code = tTrackL({
+	var element = $(tTrackL({
 			title: track.name,
 			album: track.album.name,
 			artists: artistString,
 			albumArtUrl: track.album.images[1].url,
 			score: track.score,
 			id: track.id
-		});
+	}));
 	
-	// append to list
-	$('#tracklist').append(code);
+	// add click events for voting buttons
+	element.find('.upvote').click(function () {
+		socket.emit("vote up", track.id);
+	});
+	element.find('.downvote').click(function () {
+		socket.emit("vote down", track.id);
+	});
+	
+	return element;
+}
+
+// inserts jQuery element at index and sets score array accordingly
+function insertTrackAt (trackElement, score, index)  {
+	if  (index === 0) { // if first position
+		if (scores.length == 0) { // if list is empty
+			$('#tracklist').append(trackElement);
+			scores[0] = score;
+		} else { // list is not empty
+			$('#tracklist li:eq(0)').before(trackElement);
+			scores.splice(0, 0, score);			
+		}
+	} else { // not first position
+		// insert into tracklist after the element above it
+		$('#tracklist li:eq(' + (index - 1).toString() + ')').after(trackElement);
+		// insert into score list
+		// TODO: Test if this actual works
+		scores.splice(index, 0, score);
+	}
+}
+
+// Moves track from one position to another (from and to are indexes)
+function moveTrackTo (from, to) {
+	var source = $('#tracklist li:eq(' + from.toString() + ')');
+	if (from < to)
+		to++; // removing element from DOM will cause the rest of the list to get up one index
+	insertTrackAt(source, scores[from], to);
+}
+
+// Updates the score of the track (object)
+function updateTrackScore (track, newScore) {
+	// update score value
+	var element = $('#tracklist li#' + track.id);
+	element.find('.score').html(newScore);
+	
+	// TODO: move track if needed
+	
+	// check if track needs to be moved
+	
 }
 
 $(document).on('ready', function () {
@@ -89,7 +150,10 @@ $(document).on('ready', function () {
 			tracklist.forEach(function (track) {
 				addTrack(track);
 			});
+			$(tracklist[0]).hide();
 		})
+		
+		// TODO: get upvote/downvote and update events
 		
 		$('.voting > button > svg').click(function (e) {
 			$(e.target).addClass('clicked');
