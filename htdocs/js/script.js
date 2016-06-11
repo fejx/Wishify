f7adeee7cda5e0a7bc2e218a95b06ca63fe28a83
fej <ff.jj@gmx.at>
2016-06-11 15:13:17 +0200

    - Added Vue.js to clean up code - Added track sorting - changed run command - removed handlebars (not needed anymore because of Vue.js)

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,171 +1,119 @@
-$('html').removeClass('no-js');
-$('html').addClass('js');
+var html = $('html');
+html.removeClass('no-js');
+html.addClass('js');
+
+const SEARCH_URL =
+	'https://api.spotify.com/v1/search?q=[QUERY]&type=track';
 
 var socket = io();
+var tracklist;
+var newTrackPane;
+var newTrackInput;
+var nowPlaying;
 
-// contains score values of all tracks in the order they appear in #tracklist
-var scores = [];
-
-var tNowPl;
-var tTrackL;
-
-// adds track (object) to the tracklist at the correct position
-function addTrack (track) {
-	// find position to insert
-	var i = 0;
-	while (scores[i] != undefined && // stop at end of list
-		track.score > scores[i]) // stop when track score is better than current score
-		i++;
-	
-	insertTrackAt(createTrackHtml(track), track.score, i);
-}
-
-// returns jQuery element containing li element to insert into tracklist
-function createTrackHtml (track) {
-	// create html element
-	var artistString = '';
-	track.artists.forEach(function (artist) {
-		if (artistString)
-			artistString += ', ' + artist.name;
-		else
-			artistString = artist.name;
-	});
-	
-	var element = $(tTrackL({
-			title: track.name,
-			album: track.album.name,
-			artists: artistString,
-			albumArtUrl: track.album.images[1].url,
-			score: track.score,
-			id: track.id
-	}));
-	
-	// add click events for voting buttons
-	element.find('.upvote').click(function () {
-		socket.emit("vote up", track.id);
-	});
-	element.find('.downvote').click(function () {
-		socket.emit("vote down", track.id);
-	});
-	
-	return element;
-}
-
-// inserts jQuery element at index and sets score array accordingly
-function insertTrackAt (trackElement, score, index)  {
-	if  (index === 0) { // if first position
-		if (scores.length == 0) { // if list is empty
-			$('#tracklist').append(trackElement);
-			scores[0] = score;
-		} else { // list is not empty
-			$('#tracklist li:eq(0)').before(trackElement);
-			scores.splice(0, 0, score);			
+//noinspection JSUnusedAssignment,JSUnusedGlobalSymbols
+var Vue = new Vue({
+	el: 'body',
+	data: {
+		tracks: [],
+		currentTrack: {},
+		searching: false,
+		searchResults: []
+	},
+	methods: {
+		voteUp: function (id) {
+			socket.emit("vote up", id);
+		},
+		voteDown: function (id) {
+			socket.emit("vote down", id);
+		},
+		openAddDialog: function () {
+			newTrackPane.transition({y:'0'}, 500, 'snap');
+			newTrackInput.focus();
+		},
+		searchTrack: function () {
+			if (newTrackInput.val() == '')
+				return;
+			Vue.searching = true;
+			$.get(SEARCH_URL.replace('[QUERY]', newTrackInput.val()),
+				function (results) {
+				Vue.searching = false;
+				Vue.searchResults = results.tracks.items;
+			});
+		},
+		addTrack: function (id) {
+			socket.emit("add track", id);
+			hideAddTrackPane();
 		}
-	} else { // not first position
-		// insert into tracklist after the element above it
-		$('#tracklist li:eq(' + (index - 1).toString() + ')').after(trackElement);
-		// insert into score list
-		// TODO: Test if this actually works
-		scores.splice(index, 0, score);
 	}
+});
+
+function findTrackId (track) {
+	for (var i = 0; i < Vue.tracks.length; i++)
+		if (Vue.tracks[i].id == track.id)
+			return i;
 }
 
-// Moves track from one position to another (from and to are indexes)
-function moveTrackTo (from, to) {
-	var source = $('#tracklist li:eq(' + from.toString() + ')');
-	if (from < to)
-		to++; // removing element from DOM will cause the rest of the list to get up one index
-	insertTrackAt(source, scores[from], to);
+function hideAddTrackPane () {
+	newTrackPane.transition({y:'100%'}, 500);
+	setTimeout(function () {
+		newTrackInput.val('');
+		Vue.searchResults = null;
+	}, 500);
+}
+
+// adds track (object) to the tracklist
+function addTrack (track) {
+	Vue.tracks.push(track);
+	sortTracks();
 }
 
 // Updates the score of the track (object)
 function updateTrackScore (track) {
-	// update score value
-	var element = $('#tracklist li#' + track.id);
-	element.find('.score').html(track.score);
-	
-	// TODO: move track if needed
-	
-	// check if track needs to be moved
-	
+	Vue.tracks[findTrackId(track)].score = track.score;
+	sortTracks();
+}
+
+function sortTracks () {
+	Vue.tracks.sort(function (t1, t2) {
+		return t2.score - t1.score;
+	});
 }
 
 function setCurrentPlaying (track) {
+	Vue.currentTrack = track;
+
+	// track might be null if no song is playing
 	if (track) {
-		// remove track from tracklist
-		$('#tracklist li#' + track.id).remove();
-
-		var artistString = '';
-		track.artists.forEach(function (artist) {
-			if (artistString)
-				artistString += ', ' + artist.name;
-			else
-				artistString = artist.name;
-		});
-
-		$('#nowPlaying').html(tNowPl({
-			title: track.name,
-			album: track.album.name,
-			artists: artistString,
-			albumArtUrl: track.album.images[1].url,
-			score: track.score
-		}));
-	} else { // input track is null or undefined
-		$('#nowPlaying').html(''); // clear now playing info
+		Vue.tracks.splice(findTrackId(track), 1);
+		nowPlaying.removeClass('hide').addClass('show');
 	}
+	else
+		nowPlaying.removeClass('show').addClass('hide');
 }
 
 $(document).on('ready', function () {
-	tNowPl = Handlebars.compile($('#nowPlaying-template').html());
-	tTrackL = Handlebars.compile($('#track-template').html());
-	
-	$('#newTrackButton').click(function () {
-		$('#newTrackPane').transition({y:'0'}, 500, 'snap');
-		$('#newTrackPane input').focus();
-	});
-	
-	$('#newTrackPane').click(function (e) {
-		if (e.target.id == 'newTrackPane') {
-			$('#newTrackPane').transition({y:'100%'});
-			$("#newTrackPane input").val("");
-			$("#newTrackPane ul").empty();
-			$('#newTrackPane svg').hide();
-		}
-	});
-	
-	$("#newTrackPane input").keypress(function (e) {
-        if (e.which == 13 && $("#newTrackPane input").val() != "") {
-			$('#newTrackPane svg').show();
-            $.get("https://api.spotify.com/v1/search?q=" + $("#newTrackPane input").val() + "&type=track", function (results) {
-                $('#newTrackPane svg').hide();
-				$("#newTrackPane ul").empty();
-                $("#newTrackPane ul").show();
-                results.tracks.items.forEach(function (t) {
-                    var entry = $("<li />");
-                    entry.html(t.artists[0].name + " - " + t.name);
-                    entry.click(function () {
-                        socket.emit("add track", t.id);
-                        $('#newTrackPane').transition({y:'100%'});
-                        $("#newTrackPane input").val("");
-						$("#newTrackPane ul").empty();
-                    });
-                    $("#newTrackPane ul").append(entry);
-                });
+	tracklist = $('#tracklist');
+	newTrackPane = $('#newTrackPane');
+	newTrackInput = newTrackPane.find('input');
+	nowPlaying = $('#nowPlaying');
 
-            });
-        }
-    });
-	
+	newTrackPane.click(function (e) {
+		// hide pane when clicking outside
+		if (e.target.id == 'newTrackPane')
+			hideAddTrackPane();
+	});
+
 	socket = io();
-	
+
 	socket.on('connect', function () {
-		$('html').addClass('connected');
-		
+		html.addClass('connected');
+
 		// event triggered when a track gets added by a user
 		socket.on('track added', function (t) {
         	addTrack(t);
 		});
-		
+
 		// event triggered on connecting containing all current tracks on the server
 		socket.on('tracks', function (tracklist) {
 			tracklist.forEach(function (track) {
@@ -173,8 +121,7 @@ $(document).on('ready', function () {
 			});
 			$(tracklist[0]).hide();
 		});
-		
-		// TODO: get upvote/downvote and update events
+
 		socket.on('track change', function (track) {
 			updateTrackScore(track)
 		});
@@ -182,15 +129,13 @@ $(document).on('ready', function () {
 		socket.on('now playing', function (track) {
 			setCurrentPlaying(track);
 		});
-		
+
 		$('.voting > button > svg').click(function (e) {
 			$(e.target).addClass('clicked');
-			
+
 		});
-		
-		setTimeout(function () {
-			$('html').addClass('loaded');
-		}, 1000);
+
+		html.addClass('loaded');
 	});
-	
+
 });
